425f0dfcceeb35371078b06ad85bdd757f521667
ThiagoLaredo <thiago@olatu.com.br>
2025-01-08 21:03:57 -0300

    ajustes forms

--- src/js/modules/formHandler.js
+++ src/js/modules/formHandler.js
@@ -12,9 +12,34 @@ export default class FormHandler {
         this.form.addEventListener('submit', event => this.handleSubmit(event));
     }
 
-    handleSubmit(event) {
-        // Mostra a mensagem de envio enquanto a Netlify processa o formulário
+    async handleSubmit(event) {
+        event.preventDefault(); // Evita o comportamento padrão do envio
+        const formData = new FormData(this.form);
+
+        // Mostra mensagem de carregamento
         this.showMessage('Enviando...', 'info');
+
+        try {
+            const response = await fetch('/', {
+                method: 'POST',
+                body: formData,
+                headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+            });
+
+            if (response.ok) {
+                this.showMessage(this.successMessage, 'success');
+                this.form.reset(); // Limpa os campos do formulário
+            } else {
+                throw new Error('Erro ao enviar o formulário. Tente novamente mais tarde.');
+            }
+        } catch (error) {
+            this.showMessage(error.message || this.errorMessage, 'error');
+        }
+
+        // Opcional: Oculta a mensagem após 10 segundos
+        setTimeout(() => {
+            this.responseMessage.style.display = 'none';
+        }, 10000);
     }
 
     showMessage(message, type) {
