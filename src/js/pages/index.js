90737ab59d182baad7dfa84b42b7b1fb7ede1ff2
ThiagoLaredo <thiago@olatu.com.br>
2025-02-19 15:41:13 -0300

    code-slitting

---
+++ src/js/pages/index.js
@@ -0,0 +1,95 @@
+import "../../css/global.css";
+import "../../css/header.css";
+import "../../css/introducao.css";
+import "../../css/solucoes-home.css";
+import "../../css/destaques.css";
+import "../../css/clientes.css";
+import "../../css/cases-home.css";
+import "../../css/metodologia.css";
+import "../../css/footer.css";
+import "../../css/menu-mobile.css";
+import "../../css/submenu.css";
+import "../../css/cores.css";
+import "../../css/componentes.css";
+import "../../css/fontes.css";
+import "../../css/solucoes.css";
+import "../../css/social-sidebar.css";
+import "../../css/popup.css";
+import "../../css/btn-float.css";
+
+import MenuMobile from '../modules/menu-mobile.js';
+import CarregarCases from '../modules/carregarCases.js';
+import HeaderScroll from '../modules/header-scroll.js';
+import FormHandler from '../modules/formHandler.js';
+import { initPageOpenAnimations, initScrollAnimations } from '../modules/animations.js';
+import VideoPreload from "../modules/VideoPreload.js";
+import EbookPopup from "../modules/ebookPopup.js";
+import { updateBackgrounds } from "../modules/updateBackgrounds.js";
+import EbookForm from "../modules/ebookForm.js";
+import {
+    MySwiperSolucoes, 
+    MySwiperCases 
+} from "../modules/myswiper.js";
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("DOM completamente carregado.");
+
+    // Inicializa o menu mobile com submenu integrado, caso os elementos existam
+    const menuMobile = new MenuMobile(
+        '[data-menu="logo"]',
+        '[data-menu="button"]',
+        '[data-menu="list"]',
+        '[data-menu="contato-mobile"]',
+        '[data-menu="linkedin"]',
+        '[data-menu="instagram"]'
+    );
+    if (menuMobile) {
+        console.log('MenuMobile initialized successfully');
+        menuMobile.init();
+    } else {
+        console.error('MenuMobile failed to initialize');
+    }
+
+    // Inicializa a mudança de Header ao scroll, se a classe .header existir
+    const headerEl = document.querySelector('.header');
+    if (headerEl) {
+        const headerScroll = new HeaderScroll('.header');
+        headerScroll.init();
+    }
+
+    // Animações de abertura e scroll
+    initPageOpenAnimations();
+    initScrollAnimations();
+
+    // Popup do e-book
+    new EbookPopup();
+    new EbookForm();
+    new VideoPreload();
+
+    // Sliders
+    new MySwiperSolucoes();
+    new MySwiperCases();
+
+    updateBackgrounds();
+    window.addEventListener("resize", updateBackgrounds);
+
+    new FormHandler();
+     
+    // Carrega e inicializa os cases se existirem os seletores
+    const submenuCasesEl = document.querySelector('.submenu-cases');
+    const swiperWrapperEl = document.querySelector('.cases-slides .swiper-wrapper');
+    const casesListaEl = document.querySelector('.cases-lista');
+    const caseDetalhadoEl = document.querySelector('.case-detalhado');
+
+    // Só inicia o carregamento dos cases se pelo menos um dos elementos existir
+    if (submenuCasesEl || swiperWrapperEl || casesListaEl || caseDetalhadoEl) {
+        const cases = new CarregarCases('../cases.json');
+        cases.init(
+            '.submenu-cases',     // Submenu
+            '.cases-slides .swiper-wrapper', // Swiper na index.html
+            '.cases-lista',       // Página de lista de cases (cases.html)
+            '.case-detalhado'     // Página de case detalhado (case.html)
+        );
+    }
+});
--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,3 +1,134 @@
+// const webpack = require('webpack');
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+// const TerserPlugin = require('terser-webpack-plugin');
+// const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const CopyWebpackPlugin = require('copy-webpack-plugin');
+// const path = require('path');
+
+// const pages = [
+//   'index',
+//   'solucoes',
+//   'cases',
+//   'case',
+//   'vender-mais',
+//   'se-posicionar',
+//   'gerar-conteudo',
+//   'metodologia',
+//   'contato',
+//   'obrigado',
+//   'blog',
+//   'post',
+// ];
+
+// const htmlPlugins = pages.map(page => new HtmlWebpackPlugin({
+//   template: `./src/${page}.html`,
+//   filename: `${page}.html`,
+//   minify: {
+//     removeRedundantAttributes: false,
+//   },
+// }));
+
+// module.exports = {
+//   entry: './src/js/script.js',
+//   output: {
+//     path: path.resolve(__dirname, 'dist'),
+//     filename: 'main.js',
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.js$/,
+//         exclude: /node_modules/,
+//         use: {
+//           loader: 'babel-loader',
+//           options: {
+//             presets: ['@babel/preset-env'],
+//             plugins: ['@babel/plugin-transform-runtime'],
+//           },
+//         },
+//       },
+//       {
+//         test: /\.css$/i,
+//         use: [MiniCssExtractPlugin.loader, 'css-loader'],
+//       },
+//       {
+//         test: /\.(png|svg|jpg|webp|jpeg|gif)$/i,
+//         type: 'asset/resource',
+//         generator: {
+//           filename: 'img/[name][ext][query]',
+//         },
+//       },
+//       {
+//         test: /\.(woff|woff2|eot|ttf|otf)$/i,
+//         type: 'asset/resource',
+//         generator: {
+//           filename: 'fonts/[name][ext]',
+//         },
+//       },
+//     ],
+//   },
+//   optimization: {
+//     minimize: true,
+//     minimizer: [
+//       new TerserPlugin({
+//         terserOptions: {
+//           format: {
+//             comments: false,
+//           },
+//         },
+//         extractComments: false,
+//       }),
+//       new CssMinimizerPlugin(),
+//     ],
+//   },
+//   plugins: [
+//     new CleanWebpackPlugin(),
+//     new MiniCssExtractPlugin({
+//       filename: 'style.css',
+//     }),
+//     ...htmlPlugins,
+//     new CopyWebpackPlugin({
+//       patterns: [
+//         { from: 'src/img', to: 'img' },
+//         { from: 'src/cases.json', to: 'cases.json' },
+//         { from: 'src/googlecbae4daf0a01a7b9.html', to: 'googlecbae4daf0a01a7b9.html' },
+//         { from: 'src/legendas.vtt', to: 'legendas.vtt' }, 
+//         { from: 'src/marketing-e-industria.pdf', to: 'marketing-e-industria.pdf' }, // Adiciona o PDF aqui
+//       ],
+//     }),
+//     new webpack.DefinePlugin({
+//       'process.env.CONTENTFUL_SPACE_ID': JSON.stringify('oputswbco4ug'),
+//       'process.env.CONTENTFUL_ACCESS_TOKEN': JSON.stringify('t0k-RHn4eskADHT1Gdjr27xnkXu7WqPS3NOkQdTYlZs'),
+//     }),   
+//   ],
+//   resolve: {
+//     extensions: ['.js', '.jsx', '.json'],
+//   },
+//   mode: 'production',
+//   devServer: {
+//     static: {
+//       directory: path.join(__dirname, 'dist'),
+//     },
+//     historyApiFallback: true,
+//     compress: true,
+//     port: 9000,
+//     open: true,
+//     hot: true,
+//     proxy: [
+//       {
+//         context: ['/api'],
+//         target: 'http://localhost:5005',
+//         secure: false,
+//         changeOrigin: true,
+//         timeout: 120000,
+//       },
+//     ],
+//   },
+// };
+
+
 const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
@@ -5,7 +136,6 @@ const TerserPlugin = require('terser-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-// const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
 const path = require('path');
 
 const pages = [
@@ -23,19 +153,27 @@ const pages = [
   'post',
 ];
 
+// Configura entradas dinâmicas para cada página (ex: index.js, cases.js)
+const entryPoints = pages.reduce((entries, page) => {
+  entries[page] = `./src/js/pages/${page}.js`;
+  return entries;
+}, {});
+
+// Gera instâncias do HtmlWebpackPlugin para cada página
 const htmlPlugins = pages.map(page => new HtmlWebpackPlugin({
   template: `./src/${page}.html`,
   filename: `${page}.html`,
+  chunks: [page], // Carrega apenas o JS específico de cada página
   minify: {
     removeRedundantAttributes: false,
   },
 }));
 
 module.exports = {
-  entry: './src/js/script.js',
+  entry: entryPoints,
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js',
+    filename: 'js/[name].js', // Gera um arquivo JS para cada entrada (index.js, cases.js...)
   },
   module: {
     rules: [
@@ -71,6 +209,22 @@ module.exports = {
     ],
   },
   optimization: {
+    splitChunks: {
+      chunks: 'all',
+      cacheGroups: {
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+        },
+        common: {
+          test: /[\\/]src[\\/]js[\\/](modules|utils)[\\/]/, // Ajuste para seus arquivos compartilhados
+          name: 'common',
+          minSize: 0,
+          chunks: 'all',
+        },
+      },
+    },
     minimize: true,
     minimizer: [
       new TerserPlugin({
@@ -83,11 +237,11 @@ module.exports = {
       }),
       new CssMinimizerPlugin(),
     ],
-  },
+  },  
   plugins: [
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin({
-      filename: 'style.css',
+      filename: 'css/[name].css', // CSS separado por página, se necessário
     }),
     ...htmlPlugins,
     new CopyWebpackPlugin({
@@ -95,14 +249,14 @@ module.exports = {
         { from: 'src/img', to: 'img' },
         { from: 'src/cases.json', to: 'cases.json' },
         { from: 'src/googlecbae4daf0a01a7b9.html', to: 'googlecbae4daf0a01a7b9.html' },
-        { from: 'src/legendas.vtt', to: 'legendas.vtt' }, 
-        { from: 'src/marketing-e-industria.pdf', to: 'marketing-e-industria.pdf' }, // Adiciona o PDF aqui
+        { from: 'src/legendas.vtt', to: 'legendas.vtt' },
+        { from: 'src/marketing-e-industria.pdf', to: 'marketing-e-industria.pdf' },
       ],
     }),
     new webpack.DefinePlugin({
       'process.env.CONTENTFUL_SPACE_ID': JSON.stringify('oputswbco4ug'),
       'process.env.CONTENTFUL_ACCESS_TOKEN': JSON.stringify('t0k-RHn4eskADHT1Gdjr27xnkXu7WqPS3NOkQdTYlZs'),
-    }),   
+    }),
   ],
   resolve: {
     extensions: ['.js', '.jsx', '.json'],
