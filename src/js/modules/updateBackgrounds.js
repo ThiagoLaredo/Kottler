847098f8c925fd8bd0a39b3cc1423b898fc0eec2
ThiagoLaredo <thiago@olatu.com.br>
2025-02-26 19:45:45 -0300

    otimizacao

--- src/js/modules/updateBackgrounds.js
+++ src/js/modules/updateBackgrounds.js
@@ -1,17 +1,36 @@
 export function updateBackgrounds() {
-    document.querySelectorAll(".slide-background, .slide-mask").forEach((el) => {
-      let imgName = el.getAttribute("data-bg");
-      let screenWidth = window.innerWidth;
-      let imageSize = "480w";
-  
-      if (screenWidth > 1200) {
-        imageSize = "1620w";
-      } else if (screenWidth > 600) {
-        imageSize = "1024w";
+  const elements = document.querySelectorAll(".slide-background, .slide-mask");
+
+  const loadImage = (el) => {
+    let imgName = el.getAttribute("data-bg");
+    if (!imgName) return;
+
+    let screenWidth = window.innerWidth;
+    let imageSize = "480w";
+
+    if (screenWidth > 1200) {
+      imageSize = "1620w";
+    } else if (screenWidth > 600) {
+      imageSize = "1024w";
+    }
+
+    let imageUrl = `./img/index/${imgName}-${imageSize}.webp`;
+    el.style.backgroundImage = `url('${imageUrl}')`;
+  };
+
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        loadImage(entry.target);
+        observer.unobserve(entry.target); // Para de observar depois de carregar a imagem
       }
-  
-      let imageUrl = `./img/index/${imgName}-${imageSize}.webp`;
-      el.style.backgroundImage = `url('${imageUrl}')`;
     });
-  }
-  
+  });
+
+  elements.forEach((el) => observer.observe(el));
+
+  // Atualiza imagens ao redimensionar a tela
+  window.addEventListener("resize", () => {
+    elements.forEach((el) => loadImage(el));
+  });
+}
